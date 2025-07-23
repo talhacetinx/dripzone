0e72b46f951c35b6f910fc28045fc9db76c1ddc3
vkmerd <mert32139@gmail.com>
2025-07-23 17:28:16 +0300

    prisma js updated

--- app/api/lib/prisma.js
+++ app/api/lib/prisma.js
@@ -1,14 +1,9 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
-let prisma
+const globalForPrisma = global;
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
-  }
-  prisma = global.prisma
-}
+const prisma = globalForPrisma.prisma || new PrismaClient();
 
-export default prisma
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
